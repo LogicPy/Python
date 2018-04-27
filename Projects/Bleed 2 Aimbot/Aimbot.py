fd70b8f5ad9fbb163a6a2d6f24c778284269df44
Wayne Kenney <pythogen@wayne.cool>
2018-04-26 23:01:07 -0400

    Bleed 2 Aimbot

--- Projects/Bleed 2 Aimbot/Aimbot.py
+++ Projects/Bleed 2 Aimbot/Aimbot.py
@@ -1,69 +1,31 @@
 
-# Bleed 2 - Color Aimbot
-# Wayne Kenney 2018 (Pythogen)
+#  _|_|_|                            _|_|                        _|            _|_|    _|                  _|                    _|      
+#  _|    _|    _|_|    _|  _|_|    _|        _|_|      _|_|_|  _|_|_|_|      _|    _|      _|_|_|  _|_|    _|_|_|      _|_|    _|_|_|_|  
+#  _|_|_|    _|_|_|_|  _|_|      _|_|_|_|  _|_|_|_|  _|          _|          _|_|_|_|  _|  _|    _|    _|  _|    _|  _|    _|    _|      
+#  _|        _|        _|          _|      _|        _|          _|          _|    _|  _|  _|    _|    _|  _|    _|  _|    _|    _|      
+#  _|          _|_|_|  _|          _|        _|_|_|    _|_|_|      _|_|      _|    _|  _|  _|    _|    _|  _|_|_|      _|_|        _|_|                                                                                                                                                                                                                                                                        
+# Wayne Kenney (Pythogen) - 4/25/2018
+
+#                                 ,----.___________,-,
+#         ,__                    (________________|__|
+#      __/()(_________________________/o(____)o(__  ``            _
+#     (__________________________(_(_(_(_(________Y_....-----====//
+#                   ( , , , , , , (______________)--            ((
+#                    \_____________|________|[ )) JW  ____   __  \\
+#                                   |____|    "" \.__-'`". \(__) \\\
+#                                   |____|        `""      ```"""=,))
+#                                   |    |
+#                                    ====
+
 
 # This aimbot automatically locks onto enemies by detecting pixel color locations 
-# and automatically positioning your mouse,
+# and automatically positioning your mouse. This script will continuously capture 
+# desktop screeshots and scan those screenshots immediately after. If the target's
+# pixel color is found, then the aimbot will calculate the screen coordinates and
+# finally position your mouse directly over the target.
 
 # 1) Set enemy color(s) in variable OR array 
 # 2) Run Aimbot
 
-from PIL import Image
-import pyautogui
-from time import sleep
-import sys
 
-
-# Enemy Color (Blue Robot / First Level)
-color = (29, 116, 165)
-
-# (Mouse Position / Monitor Resolution) Calculation Variables
-xCord = 0
-yCord = 0
-
-
-def main():
-	while(True):
-		cmd = raw_input("Enter Aimbot Command: ")
-		cmd = cmd.lower()
-		if cmd == "help":
-			# List of Commands
-			print "\nList of commands:\n\n  help\n  start  \n  exit\n"
-		elif cmd == "start":
-			# Execute Aimbot
-			Aimbot_GO()
-		elif cmd == "exit":
-			# Exit
-			sys.exit()
-		else:
-			print "\nInvalid command...\n"
-
-
-def Aimbot_GO():
-
-	print "\nAimbot Activated!\n"
-	# Loop for process...
-	while(True):
-		# Take screenshots (Continuous Screen Captures)
-		pic = pyautogui.screenshot()
-		 
-		# Save the image
-		pic.save('img.png') 
-
-		# Open image for analysis
-		im = Image.open("img.png","r")
-
-		# Set list with screenshot pixel colors
-		pix_val = list(im.getdata())
-
-		if color in pix_val:
-			print pix_val.index(color)
-			xCord = pix_val.index(color) / 1920
-			yCord = pix_val.index(color) / 1080
-			pyautogui.moveTo(yCord, xCord)
-			print "%s:%s" %(xCord,yCord)
-			print "True (Enemy Detected! Aiming...)"
-		else:
-			print "False (Enemy not detected)"
-
-main()
+# [Removed! Keeping private... ]
