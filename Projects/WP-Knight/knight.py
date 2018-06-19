fb8df9e055dbf7995117e2af62394567c0ed7ed9
Wayne Kenney <pythogen@wayne.cool>
2018-06-19 17:04:58 -0400

    WP-Knight
    WordPress password cracker.

--- Projects/WP-Knight/knight.py
+++ Projects/WP-Knight/knight.py
@@ -117,17 +117,6 @@ def bruteforce():
 				#Header data
 				header_data = \
 				{
-					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-					'Accept-Encoding': 'gzip, deflate, br',
-					'Accept-Language': 'en-US,en;q=0.9',
-					'Cache-Control': 'max-age=0',
-					'Connection': 'keep-alive',
-					'Content-Length': '116',
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Cookie': 'wordpress_test_cookie=WP+Cookie+check',
-					'Host': host,
-					'Upgrade-Insecure-Requests': '1',
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
 				}
 
 				# 2) Submit POST data. Initialize login
