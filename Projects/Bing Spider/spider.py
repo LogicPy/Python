3895b2758fb5c594b95c94e74c7499d7f5531a06
Wayne Kenney <thehandoflinux@gmail.com>
2018-07-22 14:13:55 -0400

    Bing Spider
    Gather/Extract a list of WordPress powered websites using Bing. This script will help you gather targets for web pen testing.

--- Projects/Bing Spider/spider.py
+++ Projects/Bing Spider/spider.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from time import sleep
 import requests
+import sys
 
 arEx = []
 arFin = []
@@ -10,16 +11,14 @@ keyword = "WordPress"
 keywords = open("keywords.txt", "r")
 kwords = keywords.read().split('\n')
 
-banner = """
-  ____  _                _____       _     _           
- |  _ \(_)              / ____|     (_)   | |          
- | |_) |_ _ __   __ _  | (___  _ __  _  __| | ___ _ __ 
- |  _ <| | '_ \ / _` |  \___ \| '_ \| |/ _` |/ _ \ '__|
- | |_) | | | | | (_| |  ____) | |_) | | (_| |  __/ |   
- |____/|_|_| |_|\__, | |_____/| .__/|_|\__,_|\___|_|   
-                 __/ |        | |                      
-                |___/         |_|       
- - WordPress Powered Website Extractor -             
+banner = """                                                             
+ _____ _            _____     _   _            ___     ___   
+| __  |_|___ ___   |   __|___|_|_| |___ ___   |_  |   |_  |  
+| __ -| |   | . |  |__   | . | | . | -_|  _|   _| |_ _ _| |_ 
+|_____|_|_|_|_  |  |_____|  _|_|___|___|_|    |_____|_|_____|
+            |___|        |_|                                  
+ - WordPress Website Gathering Tool -             
+
 """
 
 def find_between( s, first, last ):
@@ -36,6 +35,49 @@ def main():
 	global kwords
 
 	print banner
+
+	v = 0
+
+	while(v==0):
+		cmd = raw_input(" Command> ")
+		if cmd == "run":
+			scan()
+		elif cmd == "help":
+			print "\n [Commands]\n\n run - activate scanner\n rate.tester - check scan results for rate limit (use after scan) \n info - script information\n exit - exit script\n"
+		elif cmd == "info":
+			info()
+		elif cmd == "exit":
+			sys.exit()
+		elif cmd == "rate.tester":
+			print "\n [Rate Limit Tester ]\n"
+			print " To be included soon. Check back for version 1.2 ...\n"
+		else:
+			print "\n [Invalid command]\n"
+
+def info():
+	print """
+ [Information]\n\n This script allows to quickly find WordPress targets for web pen testing. 
+ The script uses Bing to search your specified keywords then it checks the 
+ content of every resulting link to determine if they are running WordPress.
+
+ Edit 'keywords.txt' and insert keywords (example: 'food blog')
+ Run script and type 'run' to begin.
+ The results will be saved to 'gathered.txt'.
+
+ Optional: Use rate limit tester to check if targets are vulnerable to 
+ dictionary attacks.
+
+ Review results and organize targets.
+ Finally use WP-Knight or WPScan to attack targets.
+
+ Note: This script is not intended for malicious purposes.
+
+ Written by LogicPy (Wayne Kenney)
+			"""
+
+def scan():
+
+	print "\n [Scanner Activated]\n"
 
 	for x in kwords:
 
