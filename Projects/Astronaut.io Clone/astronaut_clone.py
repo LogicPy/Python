8f328e4304548ebb99a9c6794ef581e02a7757f2
Wayne Kenney <thehandoflinux@gmail.com>
2020-01-02 17:07:02 -0500

    Astronaut.io Clone Software :+1:

--- Projects/Astronaut.io Clone/astronaut_clone.py
+++ Projects/Astronaut.io Clone/astronaut_clone.py
@@ -4,7 +4,7 @@ from time import sleep
 import random
 from random import randint
 import webbrowser
-
+import sys
 
 #Used for finding values between tags
 def find_between( s, first, last ):
@@ -15,37 +15,49 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
-# Randomize selection between MOV or IMG
-arr = ['MOV ','IMG ']
-arrnum = randint(0, 1)
-x = randint(0001, 9999)
+def main():
+	# Randomize selection between MOV or IMG
+	arr = ['MOV ','IMG ']
+	arrnum = randint(0, 1)
+	x = randint(0001, 9999)
 
-#randomized number with new identifier MOV or IMG
-print arr[arrnum]+ str(x)
+	#randomized number with new identifier MOV or IMG
+	print "\n" + arr[arrnum]+ str(x)
 
-# Initial generated Request
-req =  requests.get("https://www.youtube.com/results?search_query=" + arr[arrnum]+ str(x))
-print "Getting first link.."
+	# Initial generated Request
+	req =  requests.get("https://www.youtube.com/results?search_query=" + arr[arrnum]+ str(x))
+	print "Getting first link.."
 
-# Get between tags to find new refreshed link
-x = find_between(req.content,'<link rel="alternate" media="handheld" href="','"><link rel="alternate"')
-b = x
-print x 
+	# Get between tags to find new refreshed link
+	x = find_between(req.content,'<link rel="alternate" media="handheld" href="','"><link rel="alternate"')
+	b = x
+	print x 
 
-# Go to new link (Second Request)!
-req2 = requests.get(b)
+	# Go to new link (Second Request)!
+	req2 = requests.get(b)
 
-# Get (new html to check)
-r = find_between(req2.content,'<a aria-hidden="true"  href="/watch','" class=" yt-uix-sessionlink')
-print "Getting new link.\n"
+	# Get (new html to check)
+	r = find_between(req2.content,'<a aria-hidden="true"  href="/watch','" class=" yt-uix-sessionlink')
+	print "Getting new link.\n"
 
-print r
-x = r
+	print r
+	x = r
 
-# Print finalized html link! :D
-url = 'https://www.youtube.com/watch' + x
+	# Print finalized html link! :D
+	url = 'https://www.youtube.com/watch' + x
 
-print 'Now viewing: ' + url
+	print 'Now viewing: ' + url + '\n\n'
 
-# Open with webbrowser module! ;)
-webbrowser.open(url)
+	# Open with webbrowser module! ;)
+	webbrowser.open(url)
+
+while(True):
+	cmd = raw_input("Enter command ('help' for help): ")
+	if cmd == "start":
+		main()
+	elif cmd == "help":
+		print "\n Commands:\n\n   start\n   quit\n"
+	elif cmd == "quit":
+		sys.exit()
+	else:
+		print "\n Invalid command..\n"
