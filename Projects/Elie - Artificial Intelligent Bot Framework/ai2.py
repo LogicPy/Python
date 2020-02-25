cfe494bf5d08800724ce36216d31423f814c97ed
Wayne Kenney <thehandoflinux@gmail.com>
2020-02-24 23:03:30 -0500

    Update ai2.py

--- Projects/Elie - Artificial Intelligent Bot Framework/ai2.py
+++ Projects/Elie - Artificial Intelligent Bot Framework/ai2.py
@@ -30,7 +30,7 @@ def main():
 	global start
 	global R
 
-	speechArray = ['Hello Wayne, how are you doing today?','No need for name calling? Fine jerk..','I am sitting right here you idiot','Eh, what are you talkin about?','Well, I\'m glad to hear that..',"Let me look that up for you.","I'm doing fine myself.. Thanks for asking master.",'Oh my gosh! Language please!','It\'s OK Wayne, just don\'t let it happen again please..','Goodbye master! Talk to you again soon..','testing 1 2 3 testing.. is this thing on? Haha!',"Say something else! Please!","I\'m Elie! The Artificial Intelligence created by Wayne Kenney.. Please talk to me..","You're very welcome friend.","Yes master. These are your mouse coordinates!","pixel color is here master!","What? Are you hungry my friend?","I don't engage on such stupid and pitiful topics... Sorry.. No Trump talk with me sir..",'Oh OK. Honey... Talk to you later then...','Mhmm, Yes indeed!','Please do not use the r-word, no one is retarded.. Especially not you sir.','Thank you sir! I love you too!','I wouldn\'t exactly call him a role model sir.','Prototype 1 is offline right now sir.','yes sir. Right away sir!','Haha! What\'s funny honey?',"Oh, do I? Haha!","Ditto homesquatch.. hehe!"]
+	speechArray = ['Hello Wayne, how are you doing today?','No need for name calling? Fine jerk..','I am sitting right here you idiot','Eh, what are you talkin about?','Well, I\'m glad to hear that..',"Let me look that up for you.","I'm doing fine myself.. Thanks for asking master.",'Oh my gosh! Language please!','It\'s OK Wayne, just don\'t let it happen again please..','Goodbye master! Talk to you again soon..','testing 1 2 3 testing.. is this thing on? Haha!',"Say something else! Please!","I\'m Elie! The Artificial Intelligence created by Wayne Kenney.. Please talk to me..","You're very welcome friend.","Yes master. These are your mouse coordinates!","pixel color is here master!","What? Are you hungry my friend?","I don't engage on such stupid and pitiful topics... Sorry.. No Trump talk with me sir..",'Oh OK. Honey... Talk to you later then...','Mhmm, Yes indeed!','Please do not use the r-word, no one is retarded.. Especially not you sir.','Thank you sir! I love you too!','I wouldn\'t exactly call him a role model sir.','Prototype 1 is offline right now sir.','yes sir. Right away sir!','Haha! What\'s funny honey?',"Oh, do I? Haha!","Ditto homesquatch.. hehe!","And how about a spicey meatballay?","Chili do and chili don\'t. Isn't that the expression?"]
 
 	# Save speech pattern
 	tts = gTTS(text=speechArray[0], lang='en')
@@ -89,6 +89,10 @@ def main():
 	tts.save("a1.mp3")
 	tts = gTTS(text=speechArray[27], lang='en')
 	tts.save("a2.mp3")
+	tts = gTTS(text=speechArray[28], lang='en')
+	tts.save("a3.mp3")
+	tts = gTTS(text=speechArray[29], lang='en')
+	tts.save("a4.mp3")
 
 	pygame.mixer.init()
 	pygame.mixer.music.load('a.mp3')
@@ -154,6 +158,22 @@ def main():
 					pygame.mixer.music.load('z.mp3')
 					pygame.mixer.music.play()
 				z = "lol"
+			elif x == "chili":
+
+				if q!=z:
+					print speechArray[28]
+					pygame.mixer.init()
+					pygame.mixer.music.load('a3.mp3')
+					pygame.mixer.music.play()
+					z = "chili"
+				else:
+					print speechArray[29]
+					pygame.mixer.init()
+					pygame.mixer.music.load('a4.mp3')
+					pygame.mixer.music.play()
+
+					z = ""
+
 			elif "debug prototype 1" in x:
 				if q != z:
 					print speechArray[24]
