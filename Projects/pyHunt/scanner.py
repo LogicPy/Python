7559248cc6083e3d74bbbf96b2902c16c23ad192
Wayne Kenney <thehandoflinux@gmail.com>
2020-04-24 13:38:47 -0400

    scanner.py - python file

--- Projects/pyHunt/scanner.py
+++ Projects/pyHunt/scanner.py
@@ -4,7 +4,7 @@ import time
 import errno
 from threading import Thread
 import os
-
+from datetime import datetime
 
 print '  _____         _     _                _   '
 print ' (_____)       (_)   (_)        _     (_)_ '
@@ -17,6 +17,8 @@ print '         (___)              Pythogen	   \n'
 
 # Check start variable
 chStart = 0
+n=0
+arr=[]
 
 def Main():
 	global oct1
@@ -45,21 +47,32 @@ def addToClipBoard(text):
 def Scan():
 	global ipGen
 	global portNum
+	global arr
+	global n
+	global timestamp
 
 	# s as socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	# Increase scan speed by setting timeout
-	s.settimeout(0.5)
+
+	dateTimeObj = datetime.now()
+
+	# if port 80 scan faster otherwise... take it slow.. ;)
+	if portNum == str(80):
+		s.settimeout(0.25)
+	
+	else:
+		s.settimeout(0.50)
 
 	try:
 		# Connection Attempt
 		s.connect((ipGen,int(portNum)))
 	except socket.error as e:
 		# Connection Fail output
-		print ' - Connection failed on %s..' % (ipGen)
+		print ' - Connection failed on %s.. - Timestamp : %s - ' % (ipGen,dateTimeObj)
 	else:
 		# Connection Success output
-		print ' - Connection success! on %s! - [%s]' % (ipGen,i)
+		print ' - Connection success! on %s - Timestamp : %s -  [%s]' % (ipGen,i,dateTimeObj)
 		print '\n\n'
 		print ' ___                             ___ '
 		print '|  _|   _____               _   |_  |'
@@ -68,9 +81,19 @@ def Scan():
 		print '| |_   |__|  |___|___|_|_|___|   _| |'
 		print '|___|                           |___|'
 		print '\n\n'
-		#data = s.recv(1024)
+		#data = s.recv(500)
 		#print data
-		x = raw_input("Press ENTER to Copy Address and resume..\n")
+		n = n + 1
+		# current date and time
+
+		#try:
+		#	print " - Output Data: %s" % (data)
+		#except:
+		#	print " - Error on output.. Woopsi! Heh heh!"
+
+		print ' - inserted into index : ' + str(n)
+		arr.insert(n, ipGen)
+
 		# Add successful IP to clipboard
 		addToClipBoard(ipGen)
 		
@@ -83,7 +106,9 @@ def Start():
 	global ipGen
 	global i
 
-	for i in xrange(900):
+	print "\n - Configured with 9999 cycles.\n"
+
+	for i in xrange(9999):
 		ipGen = '%s.%s.%s.%s' % (oct1, oct2, oct3, oct4)
 		#ipGen = '113.66.62.24'
 		print ' [%s]Scanning %s.' % (i,ipGen)
@@ -94,7 +119,17 @@ def Start():
 
 
 def Finished():
+	global arr
+	global n
+	global ipGen
+
 	print '\n[Finished]'
+
+	print "Found %s boxes... - %s" % (str(n),ipGen)
+
+	for i in arr:
+		print i
+
 	fin = raw_input("\nScan Complete. Press ENTER to exit...\n")
 
 Main()
