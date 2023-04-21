ffa792698d4f74c19478c26a984c980f94780cf0
LogicPy <waynekenneyjr@gmail.com>
2023-04-21 12:33:17 -0400

    Clear and change directory configuration for restarting the training process now present...
    Clear and change directory configuration for restarting the training process now present. The model is now saved to the current directory in folder modelbase, for deletion for next model training session. The os call is positioned above the transformer import for a reason. It won't work if it's under that import... No idea why that's the case though. It just appears to be and people mention it on the web. Now this feature works perfectly after discovering that too!

--- AI Projects/Training Chat Models of AI/simple2.py
+++ AI Projects/Training Chat Models of AI/simple2.py
@@ -1,8 +1,11 @@
+import os
+# Reset model download by changing the directory and delete it when finished to reset the training process!
+os.environ['TRANSFORMERS_CACHE'] = os.getcwd() + '/modelbase/'
+
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, TextDataset, DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 import torch.optim as optim
-import os
 import tkinter as tk
 import platform
 import sys
@@ -67,15 +70,18 @@ commandconsole()
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
 # Load pre-trained model and tokenizer
-model_name = "gpt2-large"  # Replace this with the desired model, e.g. "gpt2-medium", "gpt2-large", or "gpt2-xl"
+model_name = "gpt2-medium"  # Replace this with the desired model, e.g. "gpt2-medium", "gpt2-large", or "gpt2-xl"
 config = GPT2Config.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name, config=config)
 learning_rate = user_input
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
 
+# Check new model directory
+os.path.isdir(os.getcwd() + '/modelbase/')
+
 # Load dataset
-train_file = "./story_dataset.json"  # Replace with the path to your training data
+train_file = "./alpaca-lora-main/templates/story_template2.json"  # Replace with the path to your training data
 train_dataset = TextDataset(tokenizer=tokenizer, file_path=train_file, block_size=128)
 
 # Initialize data collator
@@ -83,7 +89,7 @@ data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 # Set up training arguments
 training_args = TrainingArguments(
-    output_dir="output/folder",
+    output_dir="output/folder2",
     overwrite_output_dir=True,
     num_train_epochs=1,
     per_device_train_batch_size=1,
