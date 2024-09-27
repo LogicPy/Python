dfba181493e28f9d7343bef2c3559a0e8364ef0d
LogicPy <38962980+LogicPy@users.noreply.github.com>
2024-09-27 18:48:41 -0400

    I've included my threaded port scanner that searches for machine services on a random IP generated in this script.
    Great for/when looking for vulnerable services with specific port numbers.

---
+++ Cyber-Security Tools/Threaded-randomized-ip-scanner.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+import random
+import struct
+
+def scan_port(host, port, semaphore):
+    global oct1
+    global oct2
+    global oct3
+    global oct4
+    global portNum
+    global chStart
+    oct1 = random.randint(20,120)
+    oct2 = random.randint(1,255)
+    oct3 = random.randint(1,255)
+    oct4 = random.randint(1,255)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # ipGen = '%s.%s.%s.%s' % (oct1, oct2, oct3, oct4)
+    ipGen = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+
+    #print ("Scanning port %s on host %s.\n" % (port,ipGen))
+    s.settimeout(1)
+    try:
+        connection = s.connect((ipGen, port))
+        print('Port ' + str(port) + ' is open on ' + ipGen)
+        connection.close()
+    except:
+        pass
+    finally:
+        semaphore.release()
+
+def main(host):
+    semaphore = threading.Semaphore(10)
+    threads = []
+    for num_port in range(90000):
+        semaphore.acquire()
+        t = threading.Thread(target=scan_port, args=(host, port, semaphore))
+        threads.append(t)
+        t.start()
+
+if __name__ == '__main__':
+    host = "host"
+    port = int(input('Enter the port for scanning: '))
+    main(host)
