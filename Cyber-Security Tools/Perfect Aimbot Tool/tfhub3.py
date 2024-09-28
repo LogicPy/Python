d4c9757055dc6ccbace0d74bd7cc6c2749260490
LogicPy <38962980+LogicPy@users.noreply.github.com>
2024-09-28 17:16:04 -0400

    Included a new training script to verify the training settings, on top of model type verification scripts.
    The model type scripts enable the verification between training types between 'SGDClassifier' and 'LogisticRegression'.
    
    This ai vulnerability test is almost perfect. Download the pre-trained models to test out the functionality, and implement a script using 'SGDClassifier' for fine-tuning the model's knowledge with new exploits. Enjoy this foundation framework so far my friends. More updates to come soon....

---
+++ Cyber-Security Tools/Perfect Aimbot Tool/tfhub3.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import pyautogui
+import win32api, win32con, win32gui
+import cv2
+import math
+import time
+import threading
+import keyboard
+
+detector = hub.load("https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2")
+size_scale = 3
+
+def detect_quit():
+    """
+    Listen for the 'q' key press to terminate the scanning loop.
+    """
+    global running
+    keyboard.wait('q')
+    running = False
+    print("\n[INFO] 'q' pressed. Exiting the scanner...")
+
+def listen_for_exit():
+    """
+    Listen for the 'ctrl+e' key combination to terminate the scanning loop.
+    """
+    global running
+    keyboard.wait('ctrl+e')
+    running = False
+    print("\n[INFO] 'Ctrl + E' pressed. Exiting the scanner...")
+
+while True:
+    # Get rect of Window
+    hwnd = win32gui.FindWindow(None, 'AimLab_tb')
+    #hwnd = win32gui.FindWindow("UnrealWindow", None) # Fortnite
+    rect = win32gui.GetWindowRect(hwnd)
+    region = rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1]
+
+    # Get image of screen
+    ori_img = np.array(pyautogui.screenshot(region=region))
+    ori_img = cv2.resize(ori_img, (ori_img.shape[1] // size_scale, ori_img.shape[0] // size_scale))
+    image = np.expand_dims(ori_img, 0)
+    img_w, img_h = image.shape[2], image.shape[1]
+
+    # Start the quit listener threads
+    quit_thread_q = threading.Thread(target=detect_quit, daemon=True)
+    quit_thread_ctrl_e = threading.Thread(target=listen_for_exit, daemon=True)
+    quit_thread_q.start()
+    quit_thread_ctrl_e.start()
+
+    # Detection
+    result = detector(image)
+    result = {key:value.numpy() for key,value in result.items()}
+    boxes = result['detection_boxes'][0]
+    scores = result['detection_scores'][0]
+    classes = result['detection_classes'][0]
+
+    # Check every detected object
+    detected_boxes = []
+    for i, box in enumerate(boxes):
+        # Choose only person(class:1)
+        if classes[i] == 1 and scores[i] >= 0.5:
+            ymin, xmin, ymax, xmax = tuple(box)
+            if ymin > 0.5 and ymax > 0.8: # CS:Go
+            #if int(xmin * img_w * 3) < 450: # Fortnite
+                continue
+            left, right, top, bottom = int(xmin * img_w), int(xmax * img_w), int(ymin * img_h), int(ymax * img_h)
+            detected_boxes.append((left, right, top, bottom))
+            #cv2.rectangle(ori_img, (left, top), (right, bottom), (255, 255, 0), 2)
+
+    print("Detected:", len(detected_boxes))
+
+    # Check Closest
+    if len(detected_boxes) >= 1:
+        min = 99999
+        at = 0
+        centers = []
+        for i, box in enumerate(detected_boxes):
+            x1, x2, y1, y2 = box
+            c_x = ((x2 - x1) / 2) + x1
+            c_y = ((y2 - y1) / 2) + y1
+            centers.append((c_x, c_y))
+            dist = math.sqrt(math.pow(img_w/2 - c_x, 2) + math.pow(img_h/2 - c_y, 2))
+            if dist < min:
+                min = dist
+                at = i
+
+        # Pixel difference between crosshair(center) and the closest object
+        x = centers[at][0] - img_w/2
+        y = centers[at][1] - img_h/2 - (detected_boxes[at][3] - detected_boxes[at][2]) * 0.45
+
+        # Move mouse and shoot
+        scale = 1.7 * size_scale
+        x = int(x * scale)
+        y = int(y * scale)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
+        time.sleep(0.05)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        time.sleep(0.1)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+    #ori_img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
+    #cv2.imshow("ori_img", ori_img)
+    #cv2.waitKey(1)
+
+    time.sleep(0.1)
